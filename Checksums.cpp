f533978c8409c88f05ac353e0936407e4f0d770d
Glissando <justinjoelward@gmail.com>
2016-05-20 11:48:33 -0500

    Test cases added

--- Checksums.cpp
+++ Checksums.cpp
@@ -9,10 +9,15 @@
 int main()
 {
 	Checksum checksum;
-	std::vector<unsigned char> data{1, 2, 3, 4, 5, 6};
+	std::vector<unsigned char> data{0xff, 0x12, 0x2e, 0x1c, 0x55, 0x33, 0xff, 0xff, 0xbf, 0x1d, 0xff, 0xff, 0xae, 0xbc};
 
-	uint32_t check = checksum.modularSum(data);
-	bool malformed = checksum.modularSumVerify(data, check);
+	//uint32_t check = checksum.modularSum(data);
+	//data.at(0) = 2;
+	//bool malformed = checksum.modularSumVerify(data, check);
+
+	uint32_t check = checksum.parityWord(data);
+	data.at(2) = 0x2e;
+	bool malformed = checksum.parityWordVerify(data, check);
 
 	if (malformed)
 	{
